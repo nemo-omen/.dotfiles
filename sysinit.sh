e8198cf0e05c0e68a3d6dc737c39d9a89e22902a
Jeff Caldwell <nemo.omen@gmail.com>
2022-10-29 00:23:33 -0500

    Start commenting out a system init script

---
+++ sysinit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# A basic script that installs all of the apps I currently
+# use on a newly installed Arch-based system. I've been
+# distro-hopping a lot recently and I'd like to cut down the 
+# time it takes to "move in," so to speak.
+
+# Here are the apps I think I'll need:
+# Qtile
+# khal
+# rofi
+# alacritty
+# vim
+# neovim (or maybe Emacs)
+# Doom-nvim (or mayme Doom Emacs)
+# nvm (and the latest Node)
+# Deno (and Denon)
+# VS Code
+# Android Studio (at least this semester)
+# GNU Stow (run command `stow .` from within `~/.dotfiles` after the repo is cloned)
+# Powerline fonts
+# rNote
+# Xournal++ (just in case)
+
+# Repos to clone:
+# https://github.com/nemo-omen/
